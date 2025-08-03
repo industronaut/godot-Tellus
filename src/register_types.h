e6602469ead9eb3403f01fe99ec0254984b88702
Shane Hyde <shane@industronaut.com>
2025-08-04 00:02:32 +1000

    Initial commit

---
+++ src/register_types.h
@@ -0,0 +1,11 @@
+#ifndef GDEXAMPLE_REGISTER_TYPES_H
+#define GDEXAMPLE_REGISTER_TYPES_H
+
+#include <godot_cpp/core/class_db.hpp>
+
+using namespace godot;
+
+void initialise_tellus_module(ModuleInitializationLevel p_level);
+void uninitialise_tellus_module(ModuleInitializationLevel p_level);
+
+#endif // GDEXAMPLE_REGISTER_TYPES_H
